45c66e6be62fdf876a6f8d8b129c90208f93decb
Soul4Legend <yesseniacruzmb@gmail.com>
2017-06-29 09:21:07 -0700

    Add files via upload

---
+++ text_adventure_starter.py
@@ -0,0 +1,29 @@
+start = '''
+You wake up one morning and find that you aren't in your bed; you aren't even in your room.
+You're in the middle of a giant maze.
+A sign is hanging from the ivy: "You have one hour. Don't touch the walls."
+There is a hallway to your right and to your left.
+'''
+
+
+print(start)
+
+
+print("Type 'left' to go left or 'right' to go right.")
+user_input = input()
+if user_input == "left":
+    print("You decide to go left and you discover a injured mermaid on the floor..") # finished the story by writing what happens
+    print("What is a mermaid doing on the floor...espacially on a floor..where is the water?")
+    print("The mermaid looks up at you amd asks for water but you dont have any")
+
+
+
+elif user_input == "right":
+    print("You choose to go right and you come across and alien who asks for a cookie")# finished the story writing what happens
+Print(Type 'Give' to give the alien the cookie or 'ignore'to not give the cookie)
+user_imput = imput()\
+if user_imput == "Give"
+    print("PLease let this not be a IF YOU GIVE A MOUSE A COOKIE")
+    print("You give the alien the coookie and then it asks for a glass of milk")
+
+    print("here we go again...exactly like the book")
